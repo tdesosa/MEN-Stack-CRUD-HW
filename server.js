45c60d9451dc3b1f2510bb530dadd47a65952461
tdesosa <tdesosa@gmail.com>
2018-10-14 11:00:55 -0600

    Connected express to mongo

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require ('express');
 const app = express();
 const port = 3000;
 
+// Connect Express to Mongo
+require('./db/db')
+
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+// Setup Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+// Set Up a Landing Page
+app.get('/', (req, res) => {
+    res.render('landing.ejs');
+});
 
 // Server Is Listening
 app.listen(port, () => {
