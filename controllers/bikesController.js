1f080949d0075bf476606254aee5cd454ea474aa
tdesosa <tdesosa@gmail.com>
2018-10-14 12:22:51 -0600

    Created a show page

--- controllers/bikesController.js
+++ controllers/bikesController.js
@@ -36,5 +36,37 @@ router.post('/', (req, res) => {
   })
 });
 
+// Edit Route & Page
+
+router.get('/:id/edit', (req, res) => {
+
+  Bikes.findById(req.params.id, (err, foundBike) => {
+      res.render('edit.ejs', {
+        bike: foundBike,
+      });
+  });
+});
+
+// Show Route & Page
+
+router.get('/:id', (req, res) => {
+  console.log(req.params);
+  Bikes.findById(req.params.id, (err, foundBike) => {
+    console.log(foundBike, 'foundBike')
+      res.render('show.ejs', {
+        bike: foundBike
+      });
+  });
+});
+
+router.put('/:id', (req, res) => {
+  console.log(req.params.id, ' id in the put route');
+  console.log(req.body, ' this should be our form data');
+
+  Bikes.findByIdAndUpdate(req.params.id, req.body, (err, updatedModel) => {
+    res.redirect('/bikes')
+  });
+});
+
 
 module.exports = router;
